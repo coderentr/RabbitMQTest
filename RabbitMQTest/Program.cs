f676ee3e3c3d8ea406553ea39b551c13fbd4fa57
Mustafa EREN <mstafaerenn@gmail.com>
2021-12-10 00:12:48 +0300

    Add project files.

---
+++ RabbitMQTest/Program.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace RabbitMQTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person() { Name = "Mustafa", SurName = "EREN", ID = 1, BirthDate = new DateTime(1994, 05, 24),
+                Message = "İlgili aday yakınımdır :)" };
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (IConnection connection = factory.CreateConnection())
+            using (IModel channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "Coderen",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                string message = JsonConvert.SerializeObject(person);
+                var body = Encoding.UTF8.GetBytes(message);
+                channel.BasicPublish(exchange: "",
+                                     routingKey: "Coderen",
+                                     basicProperties: null,
+                                     body: body);
+                Console.WriteLine($"Gönderilen kişi: {person.Name}-{person.SurName}");
+            }
+
+            Console.WriteLine(" İlgili kişi gönderildi...");
+            Console.ReadLine();
+        }
+        public class Person
+        {
+            public int ID { get; set; }
+            public string Name { get; set; }
+            public string SurName { get; set; }
+            public DateTime BirthDate { get; set; }
+            public string Message { get; set; }
+        }   
+    }
+}
---
+++ RabbitMQ_Receive/Program.cs
@@ -0,0 +1,53 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace RabbitMQ_Receive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            List<Person> personList = new List<Person>();
+            string fileName = @"C:\TestProjeler\Logs.txt";
+            FileStream fs = new FileStream(fileName, FileMode.OpenOrCreate, FileAccess.Write);
+            using (IConnection connection = factory.CreateConnection())
+            using (IModel channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "Coderen",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+                    Person person = JsonConvert.DeserializeObject<Person>(message);
+                    personList.Add(person);
+                    File.AppendAllText(fileName, Environment.NewLine + $" Adı: {person.Name} Soyadı:{person.SurName} [{person.Message}]");
+                };
+                channel.BasicConsume(queue: "Coderen", autoAck: true, consumer: consumer);
+                fs.Close();
+                Console.ReadLine();
+            }
+        }
+        public class Person
+        {
+            public int ID { get; set; }
+            public string Name { get; set; }
+            public string SurName { get; set; }
+            public DateTime BirthDate { get; set; }
+            public string Message { get; set; }
+        }
+    }
+}
